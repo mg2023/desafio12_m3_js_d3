919fd074131cc335b5ee5dcfce65862d1d0f4a65
mg <amg.vergara@gmail.com>
2022-10-03 00:54:43 -0300

    Se agregan los puntos 1, 2 y 3

---
+++ p1/1_funcion.js
@@ -0,0 +1,13 @@
+//Funcion por declaracion
+function example(a, b, c) {
+  return a + b + c;
+}
+
+console.log(example(1, 2, 3));
+
+// Funcion por expresion
+let example1 = function (a, b, c) {
+  return a + b + c;
+};
+
+console.log(example1(1, 2, 3));
---
+++ p2/2_funcion.js
@@ -0,0 +1,22 @@
+/*Funcion por expresion*/
+suma = function(a, b){
+  return a + b;
+  }
+
+console.log(suma(1,2))
+
+
+
+/*Funcion flecha*/
+suma1 = (a,b) => a + b;
+
+console.log(suma1(1,2))
+
+
+
+
+
+
+
+
+
