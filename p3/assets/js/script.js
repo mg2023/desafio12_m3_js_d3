d80c89c870bf0ab2182119dc5e39171503f72c73
mg <amg.vergara@gmail.com>
2022-10-05 23:06:19 -0300

    Se agregan comentarios para identificar a que punto del desafio corresponde

--- p3/assets/js/script.js
+++ p3/assets/js/script.js
@@ -5,4 +5,7 @@ function pintar(color){
 
   ele = document.getElementById("ele1");
 
-  ele.addEventListener("click", ()=> { pintar("yellow");});
+  ele.addEventListener("click", ()=> { 
+    pintar("yellow");
+    console.log("Script para el punto 3.3")
+  });
